67c2a0b95e56bfc9d23ed68e5b0255ee973a212a
cr <cr@gmail.com>
2017-11-03 12:24:04 +0100

    codeMakery PARTE 3

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -7,7 +7,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -46,9 +45,6 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-
-        // Set the application icon.
-        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
         initRootLayout();
 
@@ -116,7 +112,6 @@ public class MainApp extends Application {
 			dialogStage.setTitle("Edit Person");
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.initOwner(primaryStage);
-			dialogStage.getIcons().add(new Image("file:resources/images/edit.png"));
 			Scene scene = new Scene(page);
 			dialogStage.setScene(scene);
 
